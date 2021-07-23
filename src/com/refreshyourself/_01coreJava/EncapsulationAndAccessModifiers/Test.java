9d10db594ed5b55105ca5676a35a99445dc479b5
davutbilgic <bs.davutbilgic@gmail.com>
2021-07-23 14:50:59 +0200

    Encapsulation And Access Modifiers

---
+++ src/com/refreshyourself/_01coreJava/EncapsulationAndAccessModifiers/Test.java
@@ -0,0 +1,12 @@
+package com.refreshyourself._01coreJava.EncapsulationAndAccessModifiers;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Person p1 = new Person("Jhon",25);
+
+        p1.setAge(27);
+        p1.setName("Adam");
+        System.out.println(p1.getName() +" " + p1.getAge());
+    }
+}
