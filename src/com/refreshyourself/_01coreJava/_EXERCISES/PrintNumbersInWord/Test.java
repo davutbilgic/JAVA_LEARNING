51d15ec293bd60c1c31609439afa35be3b9a8e37
davutbilgic <bs.davutbilgic@gmail.com>
2021-08-01 13:19:42 +0200

    XA

--- src/com/refreshyourself/_01coreJava/_EXERCISES/PrintNumbersInWord/Test.java
+++ src/com/refreshyourself/_01coreJava/_EXERCISES/PrintNumbersInWord/Test.java
@@ -2,10 +2,8 @@ package com.refreshyourself._01coreJava._EXERCISES.PrintNumbersInWord;
 
 public class Test {
     public static void main(String[] args) {
-        //NumberInWord.printNumberInWord(0);
-        NumberInWord.printNumberInWordWithArray(657613468345l);
-
-        NumberInWord m1 = new NumberInWord();
+        NumberInWord.printNumberInWord(0);
+        NumberInWord.printNumberInWordWithArray(657613468345L);
 
     }
 }
